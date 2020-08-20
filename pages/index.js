e45872ce231d6a9f0c24b3e7949e105a0873ef38
Metamate <jakikob@gmail.com>
2020-08-20 18:09:05 +0200

    add tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>DNP</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="bg-purple-700 text-white">
         Hi there kids.
       </main>
 
       <footer>
       </footer>
-    </div>
+    </>
   )
 }
