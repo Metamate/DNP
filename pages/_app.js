12c2b90ca25a06e8072133a79bc0f1016970a1df
Metamate <jakikob@gmail.com>
2020-08-25 09:17:37 +0200

    fix iframe size

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,8 @@ const components = {
   code: CodeBlock,
   Link: props => (
     <Link {...props} passHref />
-  )
+  ),
+  iframe: props => (<div style={{ position: 'relative', width: '100%', paddingBottom: '56.25%', height: '0' }}><iframe style={{ position: 'absolute', top: '0', left: '0', height: '100%', width: '100%' }} {...props} /></div>)
 }
 
 function MyApp({ Component, pageProps }) {
