4f0b08dd5a94a551a2a07ec30a8c8aa512cca184
Metamate <jakikob@gmail.com>
2020-08-31 13:20:55 +0200

    add images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-})
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
 module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'mdx'],
-})
+  pageExtensions: ["js", "jsx", "mdx"],
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(svg|png|jpe?g|gif|mp4)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next",
+            name: "static/media/[name].[hash].[ext]",
+          },
+        },
+      ],
+    });
+
+    if (!options.dev && options.isServer) {
+      const originalEntry = config.entry;
+
+      config.entry = async () => {
+        const entries = { ...(await originalEntry()) };
+        entries["./scripts/build-rss.js"] = "./scripts/build-rss.js";
+        return entries;
+      };
+    }
+
+    return config;
+  },
+});
